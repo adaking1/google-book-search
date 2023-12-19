8b9f2dd696dcb22b6a4659e2f87e35e43da300d6
adaking1 <137830553+adaking1@users.noreply.github.com>
2023-12-19 14:47:12 -0500

    created apollo providor in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,19 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
+import { ApolloClient, ApolloProvidor, InMemoryCache } from '@apollo/client';
 import Navbar from './components/Navbar';
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache()
+});
 
 function App() {
   return (
-    <>
+    <ApolloProvidor client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvidor>
   )
 }
 

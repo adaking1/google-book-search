24de6eb882cae0fe1182d0d706bc5f3ebbbf6e5e
adaking1 <137830553+adaking1@users.noreply.github.com>
2023-12-19 11:25:01 -0500

    created backend folder and server js file

---
+++ server/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const path = require('path');
+const db = require('./config/connection');
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+const { authMiddleware } = require('./utils/auth');
+const { typeDefs, resolvers } = require('./schemas');
+const { send } = require('process');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+const startApolloServer = async () => {
+    await server.start();
+
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
+
+    app.use('/graphql', expressMiddleware(server));
+
+    if (process.env.NODE_ENV === 'production') {
+        app.use(express.static(path.join(__dirname, '../client/dist')));
+        app.get('*', (req, res) => {
+            res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+        });
+    }
+
+    db.once('open', () => {
+        app.listen(PORT, () => {
+            console.log(`Use Graphql here http://localhost:${PORT}/graphql`);
+        });
+    });
+};
+
+startApolloServer();

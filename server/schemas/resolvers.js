32777f7c4316c7531dd5b36cb4cfeceba02c5e83
adaking1 <137830553+adaking1@users.noreply.github.com>
2023-12-19 12:50:48 -0500

    added type defs

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,23 @@
+const { Book, User } = require('../models');
+
+const resolvers = {
+    Query: {
+        getSingleUser: async () => {
+
+        }
+    },
+    Mutation: {
+        createUser: async () => {
+
+        },
+        login: async () => {
+
+        },
+        saveBook: async () => {
+
+        },
+        deleteBook: async () => {
+
+        }
+    }
+};

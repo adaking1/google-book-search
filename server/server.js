6bbdee787dc75fd2bb62836c0acc2f2180b27a3f
adaking1 <137830553+adaking1@users.noreply.github.com>
2023-12-19 14:10:58 -0500

    updated vite.config

--- server/server.js
+++ server/server.js
@@ -3,9 +3,7 @@ const path = require('path');
 const db = require('./config/connection');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
-const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
-const { send } = require('process');
 
 const PORT = process.env.PORT || 3001;
 const app = express();

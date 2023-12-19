938d770c5dcc76d335c6e6920243c79ce3ce68c2
adaking1 <137830553+adaking1@users.noreply.github.com>
2023-12-19 17:00:20 -0500

    added authservice to auth.js in utils

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
 import { ApolloClient, ApolloProvidor, InMemoryCache } from '@apollo/client';
-// import Navbar from './components/Navbar';
+import Navbar from './components/Navbar';
 
 const client = new ApolloClient({
   uri: '/graphql',
@@ -11,7 +11,7 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvidor client={client}>
-      {/* <Navbar /> */}
+      <Navbar />
       <Outlet />
     </ApolloProvidor>
   )

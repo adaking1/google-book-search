029b584515a2c6e6af76c3cf956efaadadf3e445
adaking1 <137830553+adaking1@users.noreply.github.com>
2023-12-20 12:29:20 -0500

    added saved books page

---
+++ client/src/pages/SavedBooks.jsx
@@ -0,0 +1,74 @@
+import {
+    Container,
+    Card,
+    Button,
+    Row,
+    Col
+} from 'react-bootstrap';
+import Auth from '../utils/auth';
+import { removeBookId } from '../utils/localStorage';
+import { useQuery, useMutation } from '@apollo/client';
+import { GET_ME } from '../utils/queries';
+import { REMOVE_BOOK } from "../utils/mutations";
+
+const SavedBooks = () => {
+    const { loading, data } = useQuery(GET_ME);
+    const userData = data?.me || {};
+
+    const [deleteBook, { error }] = useMutation(REMOVE_BOOK);
+    const handleDeleteBook = async (bookId) => {
+        const token = Auth.loggenIn() ? Auth.getToken() : null;
+        if (!token) {
+            return false;
+        }
+        try {
+            await deleteBook(bookId);
+            removeBookId(bookId);
+        }
+        catch (err) {
+            console.error(err);
+        }
+    };
+
+    if (!userDataLength) {
+        return <h2>LOADING...</h2>;
+    }
+
+    return (
+        <>
+          <div fluid className="text-light bg-dark p-5">
+            <Container>
+              <h1>Viewing saved books!</h1>
+            </Container>
+          </div>
+          <Container>
+            <h2 className='pt-5'>
+              {userData.savedBooks.length
+                ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
+                : 'You have no saved books!'}
+            </h2>
+            <Row>
+              {userData.savedBooks.map((book) => {
+                return (
+                  <Col md="4">
+                    <Card key={book.bookId} border='dark'>
+                      {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
+                      <Card.Body>
+                        <Card.Title>{book.title}</Card.Title>
+                        <p className='small'>Authors: {book.authors}</p>
+                        <Card.Text>{book.description}</Card.Text>
+                        <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
+                          Delete this Book!
+                        </Button>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                );
+              })}
+            </Row>
+          </Container>
+        </>
+      );
+};
+
+export default SavedBooks;
